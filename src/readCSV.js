f9cbc13c5f83c7ad7bc6204053fc2c2c31978a1c
aparzi <angeloparziale94@gmail.com>
2023-03-06 23:57:20 +0100

    first commit

--- src/readCSV.js
+++ src/readCSV.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const {parse} = require("csv-parse");
 
-fs.createReadStream("/home/aparziale/Downloads/git_commit.csv")
+fs.createReadStream("path_to_csv")
     .pipe(parse({delimiter: ",", from_line: 2}))
     .on("data", function (row) {
         console.log(row);
